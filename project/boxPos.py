9592e04f853af2448f98b1b57d656298c3c50d81
zukuzeus <danielzukowskii@gmail.com>
2018-03-17 18:43:24 +0100

    xmele są generowane zgodnie z komatybilnym formatem

--- project/boxPos.py
+++ project/boxPos.py
@@ -4,25 +4,37 @@ from xml.dom import minidom
 import xml.etree.ElementTree as ET
 import os
 
-frames = 'D:\\mgr projekt\\mgr\\project\\processing\\' + 'frameswithoutBackground'
-contours = sd.find_contours_for_frames(frames)
+framesContours = 'D:\\mgr projekt\\mgr\\project\\processing\\' + 'frameswithoutBackground'
+frames = 'D:\\mgr projekt\\mgr\\project\\processing\\' + 'frames'
+contours = sd.find_contours_for_frames(framesContours)
+xmlfolderpath = 'D:\\mgr projekt\\mgr\\project\\processing\\' + 'xml'
 
-rectanglesForImages = []
-for conts in contours:
-    tuples = []
-    for c in conts:
-        d = (x, y, w, h) = cv2.boundingRect(c)
-        tuples.append(d)
-    rectanglesForImages.append(tuples)
+
+# rectanglesForImages = []
+
+def getRectanglesFromContours(contoursForMultipleImages):
+    rectanglesForImages = []
+    for contoursInSingleImage in contoursForMultipleImages:
+        rectangles = contorusToRectangles(contoursInSingleImage)
+        rectanglesForImages.append(rectangles)
+    return rectanglesForImages
+
+
+def contorusToRectangles(contours):
+    rectangles = []
+    for c in contours:
+        d = cv2.boundingRect(c)
+        rectangles.append(d)
+    return rectangles
 
 
 def appendObjectsToXml(xml, rectangles):
-    tree = ET.ElementTree(ET.fromstring(xml))
+    tree = ET.ElementTree(xml)
     root = tree.getroot()
     for r in rectangles:
         root.append(createXmlNodeForObject(r))
 
-    finalFile = ET.tostring(tree)
+    finalFile = ET.tostring(root, encoding='unicode')
     return finalFile
 
 
@@ -40,7 +52,7 @@ def createXmlForFrame(pathToFile, imgSize):
     segmented = ET.SubElement(annotation, 'segmented')
     folder.text = os.path.basename(os.path.dirname(pathToFile))
     filename.text = os.path.basename(pathToFile)
-    path.text = pathToFile.replace("\\","/")
+    path.text = pathToFile.replace("\\", "/")
     database.text = 'Unknown'
     w, h, d = imgSize
     width.text = w.__str__()
@@ -49,8 +61,7 @@ def createXmlForFrame(pathToFile, imgSize):
 
     segmented.text = '0'
 
-    xmlfile = ET.tostring(annotation)
-
+    xmlfile = annotation
     return xmlfile
 
 
@@ -72,20 +83,38 @@ def createXmlNodeForObject(rect):
 
     x, y, w, h = rect
 
-
     xmin.text, ymin.text, xmax.text, ymax.text = (x.__str__(), y.__str__(), (x + w).__str__(), (y + h).__str__())
 
     return object
 
 
-# XMLs
-fullpaths = sd.get_sorted_filelist_full_paths(frames)
-counter = 0
-for filepath in fullpaths:
-    xmlstring = createXmlForFrame(filepath, (1920, 1080, 3))
-    # rectanglesForImages[counter]
-    xmlstringfile = appendObjectsToXml(xmlstring, rectanglesForImages[counter])
-    counter += 1
+rectanglesForImages = getRectanglesFromContours(contours)
 
-    # createXmlForFrame()
+
+fullpaths = sd.get_sorted_filelist_full_paths(frames)
+def createXmlsForFrames(filePathlist):
+    xmls = []
+    counter = 0
+    for filepath in fullpaths:
+        xmlstringfile = appendObjectsToXml(createXmlForFrame(filepath, (1920, 1080, 3)), rectanglesForImages[counter])
+        xmls.append(xmlstringfile)
+        counter += 1
+    return xmls
+
+
+# XMLs
+xmls = createXmlsForFrames(fullpaths)
+
+
+def saveXmls(xmlList, path):
+    counter = 0
+    for xml in xmlList:
+        f = open(os.path.join(path, "frame{}.xml".format(counter)), 'w')
+        f.write(xml)
+        f.close()
+        counter += 1
+
+
+saveXmls(xmls, xmlfolderpath)
 # os.path.splitext(os.path.basename("hemanth.txt"))[0] = nazwa pliku ze ścieżki
+f = 1
